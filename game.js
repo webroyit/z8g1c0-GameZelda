bd08537b5ee4788681c7c23063c969ab84bf9e2d
royweb <xytank@gmail.com>
2021-08-11 12:22:21 -0400

    Add images and load them with loadSprite()

--- game.js
+++ game.js
@@ -5,3 +5,28 @@ kaboom({
     scale: 1,
     debug: true     // To get debug message on the screen
 })
+
+// Apply URL starting path to all loadSprite
+loadRoot('https://i.imgur.com/')
+
+loadSprite('link-going-left', '1Xq9biB.png')
+loadSprite('link-going-right', 'yZIb8O2.png')
+loadSprite('link-going-down', 'tVtlP6y.png')
+loadSprite('link-going-up', 'UkV0we0.png')
+loadSprite('left-wall', 'rfDoaa1.png')
+loadSprite('top-wall', 'QA257Bj.png')
+loadSprite('bottom-wall', 'vWJWmvb.png')
+loadSprite('right-wall', 'SmHhgUn.png')
+loadSprite('bottom-left-wall', 'awnTfNC.png')
+loadSprite('bottom-right-wall', '84oyTFy.png')
+loadSprite('top-left-wall', 'xlpUxIm.png')
+loadSprite('top-right-wall', 'z0OmBd1.jpg')
+loadSprite('top-door', 'U9nre4n.png')
+loadSprite('fire-pot', 'I7xSp7w.png')
+loadSprite('left-door', 'okdJNls.png')
+loadSprite('lanterns', 'wiSiY09.png')
+loadSprite('slicer', 'c6JFi5Z.png')
+loadSprite('skeletor', 'Ei1VnX8.png')
+loadSprite('kaboom', 'o9WizfI.png')
+loadSprite('stairs', 'VghkL08.png')
+loadSprite('bg', 'u4DVsx6.png')
